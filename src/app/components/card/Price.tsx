ecd22843d1cfcee45805ef831688de50543e3699
AslamtoIbrahim <aslamtoibrahim@gmail.com>
2025-02-08 16:36:13 +0100

    changed $ dollar sign place

--- src/app/components/card/Price.tsx
+++ src/app/components/card/Price.tsx
@@ -9,7 +9,7 @@ function Price({ value = 0, style}: props) {
     <div className={`flex items-center gap-2 justify-center
                     font-manrope  text-neutral-grayishBlue font-semibold ${style}`}>
       <p className="text-neutral-darkDesaturatedBlue text-[2.5rem] font-bold  ">
-        {`${value.toFixed(2)}$ `}
+        {`$${value.toFixed(2)}`}
       </p>
       <p>/month</p>
     </div>
